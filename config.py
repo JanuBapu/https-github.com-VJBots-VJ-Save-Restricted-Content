e5f47742cd734305bb0284925551ea02a87d1013
JanuBapu <142379561+JanuBapu@users.noreply.github.com>
2024-08-28 14:30:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ API_ID = int(os.environ.get("API_ID", "25436183"))
 API_HASH = os.environ.get("API_HASH", "03fbfb7036edd6f6786e422795eb861b")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://janubapu:5uadFAdA1WQcHdtV3@cluster0.ld2ya.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://janubapu5:janubapu5@cluster0.ld2ya.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
